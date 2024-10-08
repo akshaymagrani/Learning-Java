aa74d198d30a6fd3e638b5c3ef778c4a55f53167
akshay_magrani <61657232+akshaymagrani@users.noreply.github.com>
2024-10-08 21:06:58 +0530

    Program to add elements of a built-in array

---
+++ src/SumOfArrayElements.java
@@ -0,0 +1,14 @@
+
+public class SumOfArrayElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] numbers = {1, 2, 3};
+		int sum = 0;
+		for (int n: numbers) {
+			sum += n;
+		}
+		System.out.println("Sum of numbers in array: " + sum);
+	}
+
+}
