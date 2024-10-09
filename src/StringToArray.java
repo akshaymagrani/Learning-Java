2e564afd19a155c3be8d7f36bc2001a5cb6813d9
akshay_magrani <61657232+akshaymagrani@users.noreply.github.com>
2024-10-09 21:25:30 +0530

    Program to convert string to array.

---
+++ src/StringToArray.java
@@ -0,0 +1,12 @@
+
+public class StringToArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sentence = "Hello";
+		char[] arrayFromSentence = sentence.toCharArray();
+		
+		System.out.println(arrayFromSentence[0]);
+	}
+
+}
